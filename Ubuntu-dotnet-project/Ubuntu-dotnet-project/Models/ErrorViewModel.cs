c2b567d87e1b8d63979f8b997a2dc13212af8368
peta999 <101709235+peta999@users.noreply.github.com>
2022-08-22 15:27:19 +0200

    init project

---
+++ Ubuntu-dotnet-project/Ubuntu-dotnet-project/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Ubuntu_dotnet_project.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
